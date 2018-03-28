fd51b58c2ddc30bf0da0031a069c0037c84f1fe6
gabrielova.veronika@gmail.com <gabrielova.veronika@gmail.com>
2018-03-29 01:10:37 +0200

    Fix importing .scss files with Flow

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -88,9 +88,9 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     // `web` extension prefixes have been added for better support
     // for React Native Web.
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx', '.scss'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -121,7 +121,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -149,7 +149,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
--- src/components/GoToWordsList.jsx
+++ src/components/GoToWordsList.jsx
@@ -1,10 +1,6 @@
-//@flow
+import React from 'react';
 
-import * as React from 'react';
-import './GoToWordsList.scss';
-// type Props = {|
-
-// |};
+import './GoToWordsList.scss'
 
 export const GoToWordsList = () => (
   <div className="words-list-container">
